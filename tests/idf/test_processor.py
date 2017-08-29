fcff0855059ee260843defae4a1c8cfac2fd1e1c
Edwin Lee <leeed2001@gmail.com>
2017-08-29 08:26:18 -0600

    Add further tests

--- tests/idf/test_processor.py
+++ tests/idf/test_processor.py
@@ -103,38 +103,38 @@ something without a semicolon !- with a comment
 
 
 class TestIDFProcessingViaFile(unittest.TestCase):
-    #     def test_valid_idf_file_simple(self):
-    #         cur_dir = os.path.dirname(os.path.realpath(__file__))
-    #         idf_path = os.path.join(cur_dir, "..", "..", "versions", "8.6", "1ZoneEvapCooler.idf")
-    #         processor = IDFProcessor()
-    #         idf_structure = processor.process_file_given_file_path(idf_path)
-    #         self.assertEquals(80, len(idf_structure.objects))
-    #
-    # def test_valid_idf_file_complex(self):
-    #     cur_dir = os.path.dirname(os.path.realpath(__file__))
-    #     idf_path = os.path.join(cur_dir, "..", "..", "versions", "8.6", "RefBldgLargeHotelNew2004.idf")
-    #     processor = IDFProcessor()
-    #     idf_structure = processor.process_file_given_file_path(idf_path)
-    #     self.assertEquals(1136, len(idf_structure.objects))
+    def test_valid_idf_file_simple(self):
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        idf_path = os.path.join(cur_dir, "1ZoneEvapCooler.idf")
+        processor = IDFProcessor()
+        idf_structure = processor.process_file_given_file_path(idf_path)
+        self.assertEquals(80, len(idf_structure.objects))
+
+    def test_valid_idf_file_complex(self):
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        idf_path = os.path.join(cur_dir, "RefBldgLargeHotelNew2004.idf")
+        processor = IDFProcessor()
+        idf_structure = processor.process_file_given_file_path(idf_path)
+        self.assertEquals(1136, len(idf_structure.objects))
 
     def test_missing_idf(self):
         cur_dir = os.path.dirname(os.path.realpath(__file__))
-        idf_path = os.path.join(cur_dir, "..", "..", "support", "transition_files", "NotReallyThere.idf")
+        idf_path = os.path.join(cur_dir, "NotReallyThere.idf")
         processor = IDFProcessor()
         with self.assertRaises(ProcessingException):
             processor.process_file_given_file_path(idf_path)
-    #
-    # def test_blank_idf(self):
-    #     cur_dir = os.path.dirname(os.path.realpath(__file__))
-    #     idf_path = os.path.join(cur_dir, "..", "..", "versions", "other", "Blank.idf")
-    #     processor = IDFProcessor()
-    #     with self.assertRaises(ProcessingException):  # should fail because needs version object at least
-    #         processor.process_file_given_file_path(idf_path)
-    #
-    # def test_minimal_idf(self):
-    #     cur_dir = os.path.dirname(os.path.realpath(__file__))
-    #     idf_path = os.path.join(cur_dir, "..", "..", "versions", "other", "Minimal.idf")
-    #     processor = IDFProcessor()
-    #     idf_structure = processor.process_file_given_file_path(idf_path)
-    #     self.assertEquals(1, len(idf_structure.objects))
-    #     self.assertAlmostEqual(idf_structure.version_float, 1.1, 1)
+
+    def test_blank_idf(self):
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        idf_path = os.path.join(cur_dir, "Blank.idf")
+        processor = IDFProcessor()
+        with self.assertRaises(ProcessingException):  # should fail because needs version object at least
+            processor.process_file_given_file_path(idf_path)
+
+    def test_minimal_idf(self):
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        idf_path = os.path.join(cur_dir, "Minimal.idf")
+        processor = IDFProcessor()
+        idf_structure = processor.process_file_given_file_path(idf_path)
+        self.assertEquals(1, len(idf_structure.objects))
+        self.assertAlmostEqual(idf_structure.version_float, 1.1, 1)
