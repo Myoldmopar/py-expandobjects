bb596962a7e92fff10a75e2ee1aaca7d59d97578
Edwin Lee <leeed2001@gmail.com>
2017-08-29 08:08:32 -0600

    Flake8

--- tests/test_expander.py
+++ tests/test_expander.py
@@ -7,12 +7,12 @@ class TestExpander(unittest.TestCase):
     def test_simple_expansion(self):
         input_object = 'Version,8.7;HVACTemplate:Something,Field1,Field2;'
         em = ExpansionManager()
-        expected_text = """\
-Version,
-  8.7;                     !- 
-
-Something,NewField1;
-
-"""
+        expected_text = '\n'.join([
+            "Version,",
+            "  8.7;                     !- ",
+            "",
+            "Something,NewField1;",
+            "",
+            ""])
         output_text = em.expand(input_object)
         self.assertEqual(expected_text, output_text)
